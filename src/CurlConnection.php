fd3b6dab2e6b7896744dec9fb68950b166ae0096
zardsama <zardsama@naver.com>
2021-10-18 13:30:20 +0900

    Update CurlConnection.php
    user-Agent 변경

--- src/CurlConnection.php
+++ src/CurlConnection.php
@@ -22,7 +22,7 @@
 			}
 
 			$this->curl = curl_init();
-			$this->agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.0.04506.30)';
+            $this->agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.229 Whale/2.10.123";
 		}
 
 		public function setHeader($header) {
